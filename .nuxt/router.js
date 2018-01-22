684aff9b46238256a661c9a99172784ccc9fec16
chrisprosise <1337074512@qq.com>
2018-01-22 08:12:29 +0800

    添加后台

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,17 +4,19 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const _1f4db736 = () => import('..\\pages\\index.vue' /* webpackChunkName: "pages\\index" */).then(m => m.default || m)
-const _19a78aa5 = () => import('..\\pages\\appoint.vue' /* webpackChunkName: "pages\\appoint" */).then(m => m.default || m)
-const _586d864a = () => import('..\\pages\\rules.vue' /* webpackChunkName: "pages\\rules" */).then(m => m.default || m)
-const _740d5884 = () => import('..\\pages\\contact.vue' /* webpackChunkName: "pages\\contact" */).then(m => m.default || m)
-const _0825795e = () => import('..\\pages\\backappoint.vue' /* webpackChunkName: "pages\\backappoint" */).then(m => m.default || m)
-const _50bd0b33 = () => import('..\\pages\\admin.vue' /* webpackChunkName: "pages\\admin" */).then(m => m.default || m)
-const _25be4256 = () => import('..\\pages\\noticecont.vue' /* webpackChunkName: "pages\\noticecont" */).then(m => m.default || m)
-const _00d4471e = () => import('..\\pages\\about.vue' /* webpackChunkName: "pages\\about" */).then(m => m.default || m)
 const _0c006873 = () => import('..\\pages\\bulletin.vue' /* webpackChunkName: "pages\\bulletin" */).then(m => m.default || m)
-const _0f1174b4 = () => import('..\\pages\\details.vue' /* webpackChunkName: "pages\\details" */).then(m => m.default || m)
-const _0784d879 = () => import('..\\pages\\lists.vue' /* webpackChunkName: "pages\\lists" */).then(m => m.default || m)
+const _fa3b4238 = () => import('..\\pages\\test\\index.vue' /* webpackChunkName: "pages\\test\\index" */).then(m => m.default || m)
+const _a2e20972 = () => import('..\\pages\\rules\\index.vue' /* webpackChunkName: "pages\\rules\\index" */).then(m => m.default || m)
+const _25be4256 = () => import('..\\pages\\noticecont.vue' /* webpackChunkName: "pages\\noticecont" */).then(m => m.default || m)
+const _740d5884 = () => import('..\\pages\\contact.vue' /* webpackChunkName: "pages\\contact" */).then(m => m.default || m)
+const _785abfa0 = () => import('..\\pages\\device\\index.vue' /* webpackChunkName: "pages\\device\\index" */).then(m => m.default || m)
 const _621a6c66 = () => import('..\\pages\\pug.vue' /* webpackChunkName: "pages\\pug" */).then(m => m.default || m)
+const _31bed9ef = () => import('..\\pages\\admin\\index.vue' /* webpackChunkName: "pages\\admin\\index" */).then(m => m.default || m)
+const _4263db90 = () => import('..\\pages\\device\\details.vue' /* webpackChunkName: "pages\\device\\details" */).then(m => m.default || m)
+const _2a18f684 = () => import('..\\pages\\admin\\user.vue' /* webpackChunkName: "pages\\admin\\user" */).then(m => m.default || m)
+const _dfd42552 = () => import('..\\pages\\admin\\backappoint.vue' /* webpackChunkName: "pages\\admin\\backappoint" */).then(m => m.default || m)
+const _2dd16e48 = () => import('..\\pages\\admin\\announce.vue' /* webpackChunkName: "pages\\admin\\announce" */).then(m => m.default || m)
+const _6394208f = () => import('..\\pages\\device\\appoint.vue' /* webpackChunkName: "pages\\device\\appoint" */).then(m => m.default || m)
 
 
 
@@ -56,29 +58,19 @@ export function createRouter () {
 			name: "index"
 		},
 		{
-			path: "/appoint",
-			component: _19a78aa5,
-			name: "appoint"
+			path: "/bulletin",
+			component: _0c006873,
+			name: "bulletin"
+		},
+		{
+			path: "/test",
+			component: _fa3b4238,
+			name: "test"
 		},
 		{
 			path: "/rules",
-			component: _586d864a,
+			component: _a2e20972,
 			name: "rules"
-		},
-		{
-			path: "/contact",
-			component: _740d5884,
-			name: "contact"
-		},
-		{
-			path: "/backappoint",
-			component: _0825795e,
-			name: "backappoint"
-		},
-		{
-			path: "/admin",
-			component: _50bd0b33,
-			name: "admin"
 		},
 		{
 			path: "/noticecont",
@@ -86,29 +78,49 @@ export function createRouter () {
 			name: "noticecont"
 		},
 		{
-			path: "/about",
-			component: _00d4471e,
-			name: "about"
+			path: "/contact",
+			component: _740d5884,
+			name: "contact"
 		},
 		{
-			path: "/bulletin",
-			component: _0c006873,
-			name: "bulletin"
-		},
-		{
-			path: "/details",
-			component: _0f1174b4,
-			name: "details"
-		},
-		{
-			path: "/lists",
-			component: _0784d879,
-			name: "lists"
+			path: "/device",
+			component: _785abfa0,
+			name: "device"
 		},
 		{
 			path: "/pug",
 			component: _621a6c66,
 			name: "pug"
+		},
+		{
+			path: "/admin",
+			component: _31bed9ef,
+			name: "admin"
+		},
+		{
+			path: "/device/details",
+			component: _4263db90,
+			name: "device-details"
+		},
+		{
+			path: "/admin/user",
+			component: _2a18f684,
+			name: "admin-user"
+		},
+		{
+			path: "/admin/backappoint",
+			component: _dfd42552,
+			name: "admin-backappoint"
+		},
+		{
+			path: "/admin/announce",
+			component: _2dd16e48,
+			name: "admin-announce"
+		},
+		{
+			path: "/device/appoint",
+			component: _6394208f,
+			name: "device-appoint"
 		}
     ],
     fallback: false
