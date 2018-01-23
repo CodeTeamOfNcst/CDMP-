ad9b40d532bba1751a5c0b4ee288154cf94026b5
王洋洋 <1028658627@qq.com>
2018-01-23 13:10:55 +0800

    规章制度

---
+++ pages/rules/ruletwo.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="">
+        <div class="headerName">
+            <div class="leftSty"></div>
+            <span class="bullCont">
+                <p>规章制度></p>
+                <p class="bulletinName">&nbsp;&nbsp; 测试中心化学药品管理规定</p>
+            </span>
+        </div>
+        <el-row class="headerline">
+            <el-col :span="24"></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <div class="grid-content bg-purple-dark detailsCont">
+                    <p>  1.实验室内设专（兼）职的管理人员，对所有购进药品进行登记，妥善存储。<br/>
+
+                        2.所有化学药品、试剂等未经许可不得擅自带出实验室。<br/>
+
+                        3.自制试剂溶液应根据试剂的性质及用量放于有塞或有盖的试剂瓶中，见光易分解的试剂要装入棕色瓶中；并贴上标签，注明名称，浓度及配制日期。<br/>
+
+                        4.标准溶液配置完成后需放入冰箱内保存，保存期限为六个月。<br/>
+
+                        5.实验室人员应对制备、测试、传递过程中的样品加以防护，避免非检验性损坏。<br/>
+
+                        6.每月对化学药品、试剂、样品进行使用盘点，确保所有化学药品、试剂均得到妥善使用与保存，并统一收集已测试样品及废液，固形废物置于废物桶、废液需倒入废液箱，禁止随意乱扔杂物或将废液倒入水槽中。<br/>
+
+                        7.使用人员在使用过程中要严格执行操作规程，注意安全，防止意外事故的发生。<br/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                    </p>
+                </div>
+            </el-col>
+        </el-row>
+    </section>
+</template>
+
+<style scoped>
+    .bullCont p{
+        width: auto;
+        float: left;
+    }
+    .detailsCont{
+        width: 100%;
+        margin-top: 20px;
+        min-height: 700px;
+        height:auto;
+    }
+    .bulletinName{
+        color:#7f828b;
+        overflow: hidden;
+    }
+</style>
+
+<script>
+
+
+</script>
