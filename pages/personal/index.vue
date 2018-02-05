af92f492a32c32e853e46a2a897c86dc8331b0e7
zhanglingxue <1050080567@qq.com>
2018-02-05 11:37:24 +0800

    .

--- pages/personal/index.vue
+++ pages/personal/index.vue
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-<!--<template>-->
-    <!--<section class="container">-->
-        <!--<div>-->
-            <!--<el-tabs :tab-position="tabPosition" style="height: 200px;">-->
-                <!--<el-tab-pane label="正在使用仪器">-->
-                    <!--<div v-for="(data,index )in tableData">-->
-                        <!--<div class="history">-->
-                            <!--<img :src="data.device.imgFilePath">-->
-                            <!--<div class="hisCont">-->
-                                <!--<p>结点赋给和大局观奋斗的鬼地方</p>-->
-                                <!--<div class="startTime">开始时间:{{data.startDate}}</div>-->
-                                <!--<div class="startTime">结束时间:{{data.endDate}}</div>-->
-                                <!--<div class="startTime">设备类型:{{data.deviceType.name}}</div>-->
-                            <!--</div>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</el-tab-pane>-->
-                <!--<el-tab-pane label="历史预约记录">-->
-                    <!--<el-table-->
-                            <!--:data="tableData"-->
-                            <!--stripe-->
-                            <!--style="width: 100%">-->
-                        <!--<el-table-column-->
-                                <!--prop="apply.startDate"-->
-                                <!--label="开始时间"-->
-                                <!--width="180"-->
-                                <!--align="left">-->
-                        <!--</el-table-column>-->
-                        <!--<el-table-column-->
-                                <!--prop="apply.endDate"-->
-                                <!--label="结束时间"-->
-                                <!--width="180"-->
-                                <!--align="left">-->
-                        <!--</el-table-column>-->
-                        <!--<el-table-column-->
-                                <!--prop="device.name"-->
-                                <!--label="仪器名称"-->
-                                <!--align="left">-->
-                        <!--</el-table-column>-->
-                        <!--<el-table-column-->
-                                <!--prop="deviceType.name"-->
-                                <!--label="仪器类型"-->
-                                <!--width="350"-->
-                                <!--align="left">-->
-                        <!--</el-table-column>-->
-                        <!--<el-table-column-->
-                                <!--label="操作"-->
-                                <!--width="120"-->
-                                <!--align="left">-->
-                            <!--<template slot-scope="scope">-->
-                                <!--<el-button type="text" @click="open2">取消</el-button>-->
-                            <!--</template>-->
-                        <!--</el-table-column>-->
-                    <!--</el-table>-->
-                <!--</el-tab-pane>-->
-            <!--</el-tabs>-->
-        <!--</div>-->
-    <!--</section>-->
-<!--</template>-->
-=======
 <template>
     <section class="container">
         <div v-if="result">
@@ -122,44 +61,44 @@
         </div>
     </section>
 </template>
->>>>>>> 256eac3c62d575ceed839a2adb0d4f2647e4b0ed
 
-<!--<style scoped>-->
-    <!--.history{-->
-        <!--width: 30%;-->
-        <!--min-width: 287px;-->
-        <!--margin-bottom: 10px;-->
-        <!--margin-left: 2.5%;-->
-        <!--float: left;-->
-        <!--display: block;-->
-    <!--}-->
-    <!--img{-->
-        <!--width: 40%;-->
-        <!--height: 100%;-->
-        <!--float: left;-->
-        <!--background-size: 100% 100%;-->
-    <!--}-->
-    <!--.hisCont{-->
-        <!--width: 60%;-->
-        <!--float: right;-->
-        <!--height: 80%;-->
-        <!--margin-top: 10%;-->
-    <!--}-->
-    <!--.hisCont p{-->
-        <!--width: 100%;-->
-        <!--height: 20px;-->
-        <!--overflow: hidden;-->
-        <!--text-overflow: ellipsis;-->
-        <!--white-space: nowrap;-->
-    <!--}-->
-    <!--.startTime{-->
-        <!--width: 100%;-->
-        <!--height: 20px;-->
-        <!--margin-top: 5px;-->
-    <!--}-->
-<!--</style>-->
 
-<<<<<<< HEAD
+<style scoped>
+    .history{
+        width: 30%;
+        min-width: 287px;
+        margin-bottom: 10px;
+        margin-left: 2.5%;
+        float: left;
+        display: block;
+    }
+    img{
+        width: 40%;
+        height: 100%;
+        float: left;
+        background-size: 100% 100%;
+    }
+    .hisCont{
+        width: 60%;
+        float: right;
+        height: 80%;
+        margin-top: 10%;
+    }
+    .hisCont p{
+        width: 100%;
+        height: 20px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    .startTime{
+        width: 100%;
+        height: 20px;
+        margin-top: 5px;
+    }
+</style>
+
+
 <!--<script>-->
     <!--import axios from 'axios'-->
     <!--export default {-->
@@ -240,7 +179,6 @@
         <!--}-->
     <!--}-->
 <!--</script>-->
-=======
 <script>
     import axios from 'axios'
     export default {
@@ -287,9 +225,9 @@
                         }
                     }
                 }
-            }else{//客户端渲染的情况，当然也需要
+            }else{
+                //客户端渲染的情况，当然也需要
             }
-            
         }
     }
 </script>
