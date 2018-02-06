0ab58eb9c39469b8ee91400ceb31acb405f5c520
chrisprosise <1337074512@qq.com>
2018-02-06 14:54:21 +0800

    修改bug

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 export default () => {
     const host =  process.env.HOST || '127.0.0.1';
     const port =  process.env.PORT || 3000;
+    
     axios.defaults.baseURL = `http://${host}:${port}`
     axios.defaults.withCredentials = true
 }   
