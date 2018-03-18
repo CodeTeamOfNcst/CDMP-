01a1e26b93c3f20948c08d4bb69aa8fd58041e7c
wangyangyangsky <1028658627@qq.com>
2018-03-18 16:09:06 +0800

    平台使用规范

--- pages/clcresources/index.vue
+++ pages/clcresources/index.vue
@@ -2,11 +2,16 @@
     <div class="all">
       <el-tabs :tab-position="tabPosition" type = "card" style="height: 700px;">
         <el-tab-pane label="机房环境">
-          <!-- <section class="container">  -->
+          <el-row>
+          <el-col :span="24"><div class="grid-content bg-purple-dark">
             <div class="leftSty"></div>
             <span class="bullCont"><i class="el-icon-d-arrow-right"></i>机房环境</span>
-          <!-- </section> -->
-          <el-row class="headerline"></el-row>    
+          </div>
+          </el-col>
+          <el-col :span="16" :offset="5">
+            <h2>24小时内机房环境温湿度曲线（五分钟内采集的平均温湿度）</h2>
+          </el-col>
+          </el-row>
         </el-tab-pane>
         <el-tab-pane label="计算资源">
           <!-- <section class="container">  -->
--- pages/usedirection/index.vue
+++ pages/usedirection/index.vue
@@ -53,6 +53,16 @@
         <div class="leftSty"></div>
         <span class="bullCont"><i class="el-icon-d-arrow-right"></i>平台使用规范</span>   
         <el-row class="headerline"></el-row>  
+         <div class="contentList">
+            <el-row class="strip">
+                <!-- <div v-for="rule in rulesDetail">
+                    <el-col :span="24"><div class="grid-content bg-purple-dark"><a :href='"/notice/" + rule.id'>{{ rule.title }} 发布时间：{{ rule.publishDate }} </a></div></el-col>
+                </div> -->
+                <!-- <div v-for="rule in rulesDetail"> -->
+                    <el-col :span="24"><div class="grid-content bg-purple-dark"> <a href="#">云计算资源及仪器预约平台使用守则 发布时间： 2018-01-01 </a></div></el-col>
+                <!-- </div> -->
+            </el-row>
+        </div>
     </el-tab-pane>
     <!-- <el-tab-pane label="仪器资源简介">定时任务补偿</el-tab-pane> -->
   </el-tabs>
@@ -78,6 +88,33 @@
 .bullCont{
     font-size:14px;
  }
+ a{
+        text-decoration: none;
+    }
+    .contentList{
+        width: 100%;
+        height:650px;
+    }
+    .paging{
+        width: 40%;
+        margin-left: 30%;
+    }
+    .strip{
+        margin-left:30px;
+        width: 80%;
+        height: 35px;
+        /*border: 1px solid #2e2f30;*/
+    }
+    .grid-content{
+        float:left;
+        line-height: 35px;
+    }
+    .grid-content>a{
+        color:#3675a8;
+    }
+    .grid-content>a:hover{
+        color:black;
+    }
 </style>
 <script>
   export default {
