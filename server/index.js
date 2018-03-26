c187a42cc46428497f032c7b512af8d10590f0d7
3chrisprosise <1337074512@qq.com>
2018-03-26 18:05:01 +0800

    add

--- server/index.js
+++ server/index.js
@@ -5,86 +5,81 @@ import {
 } from 'nuxt'
 import Assert from 'assert'
 import Cors from 'koa2-cors'
+import logger from 'koa-logger'
+import koaStatic from 'koa-static'
+import path from 'path'
+import session from 'koa2-session-store'
+import bodyParser from 'koa-bodyparser'
 
+import database from './dbconfig/dbinit'
 
-const logger = require('koa-logger')
-const koaStatic = require('koa-static')
-const session = require('koa2-session-store')
-const bodyParser = require('koa-bodyparser')
-
-const database = require('./dbconfig/dbinit')
 const router = require('./router')()
-const jwt = require('koa-jwt')
 
-async function start() {
-    const app = new Koa()
+const start = async () => {
+  const app = new Koa()
+  const host = process.env.HOST || '127.0.0.1'
+  const port = process.env.PORT || 3000
 
-    const host =  process.env.HOST || '127.0.0.1';
-    const port =  process.env.PORT || 3000;
+  app.keys = ['dsadasdsada*-*/*-/sda*-d/as*-d/w21*-/31-*sda-d*']
+  /**
+   ** Test connecton
+  */
+  try {
+    await database.sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (err) {
+    console.error('Unable to connect to the database', err)
+    Assert.ok(false, 'Unable to connect to the database')
+  }
 
-    app.keys = ['dsadasdsada*-*/*-/sda*-d/as*-d/w21*-/31-*sda-d*'];
+  if (process.env.NODE_ENV === 'development') {
     /**
-     ** Test connecton
-     */
-    try {
-        await database.sequelize.authenticate()
-        console.log('Connection has been established successfully.')
-    } catch (err) {
-        console.error('Unable to connect to the database', err)
-        Assert.ok(false, 'Unable to connect to the database')
+    ** Sync DB
+    */
+    // await database.sequelize.sync()
+    // console.log("Database Sync successfully")
+  }
+  // init middleware
+  app.use(logger())
+  app.use(Cors({
+    'credentials': true
+  }))  // 允许跨域访问
+  app.use(bodyParser())
+  app.use(session({
+    name: 'sessid',
+    secret: '*2-131/*-',
+    cookie: {
+      maxAge: 23333 // just example
     }
+  }))
+  app.use(koaStatic(path.resolve(__dirname, '/uploads')))
+  app.use(router.routes()).use(router.allowedMethods())
+  // Import and Set Nuxt.js options
+  let config = require('../nuxt.config.js')
+  config.dev = !(app.env === 'production')
 
-    if (process.env.NODE_ENV === "development") {
-        /**
-         ** Sync DB
-         */
-        // await database.sequelize.sync()
-        // console.log("Database Sync successfully")
-    }
-    // init middleware
-    app.use(logger())
-    app.use(Cors({
-        'credentials': true
-    })) //允许跨域访问
-    app.use(bodyParser())
-    app.use(session({
-        name: 'sessid',
-        secret: "*2-131/*-",
-        cookie: {
-            maxAge: 23333 // just example
-        }
-    }))
-    
-    app.use(koaStatic(__dirname + '/uploads'))
-    app.use(router.routes()).use(router.allowedMethods())
-    
-    // Import and Set Nuxt.js options
-    let config = require('../nuxt.config.js')
-    config.dev = !(app.env === 'production')
-
-    // Instantiate nuxt.js
-    const nuxt = new Nuxt(config)
-    
-    // Build in development
-    if (config.dev) {
-        const builder = new Builder(nuxt)
-        await new Builder(nuxt).build()
-    }
-    
-    app.use(async (ctx, next) => {
-        await next()
-        ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-        return new Promise((resolve, reject) => {
-            ctx.res.on('close', resolve)
-            ctx.res.on('finish', resolve)
-            nuxt.render(ctx.req, ctx.res, promise => {
-                // nuxt.render passes a rejected promise into callback on error.
-                promise.then(resolve).catch(reject)
-            })
-        })
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config)
+  // Build in development
+  if (config.dev) {
+    // const builder = new Builder(nuxt)
+    await new Builder(nuxt).build()
+  }
+  app.use(async (ctx, next) => {
+    await next()
+    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+    return new Promise((resolve, reject) => {
+      ctx.res.on('close', resolve)
+      ctx.res.on('finish', resolve)
+      nuxt.render(ctx.req, ctx.res, promise => {
+      // nuxt.render passes a rejected promise into callback on error.
+        promise.then(resolve).catch(reject)
+      })
     })
+  })
 
-    app.listen(port, host)
-    console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+  app.listen(port, host)
+  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
 }
+
 start()
--- server/router.js
+++ server/router.js
@@ -1,14 +1,6 @@
 import Router from 'koa-router'
-const multer = require('koa-multer');
-const upload = multer({
-    dest: './static/uploads'
-});
-
 import {
     testGetData,
-    AddUser,
-    addDataToDataBase,
-    getOrCreateData,
     createData
 } from './service/test_service'
 import {
@@ -68,84 +60,82 @@ import {
     deleteTempFile,
     copyTempFileToDir
 } from './service/upload'
-import {
-    checkFile,
-    getVedioStream
-} from './service/video'
 
-
+import { checkFile, getVedioStream } from './service/video'
 /**
- * 全局 service router 定义
- */
+* 全局 service router 定义
+*/
+
+const multer = require('koa-multer');
+const upload = multer({
+  dest: './static/uploads'
+})
 module.exports = () => {
+  const router = new Router({
+    prefix: '/api'
+  })
 
-    const router = new Router({
-        prefix: '/api'
-    });
-
-    router.get('/test', testGetData);
-    router.get('/initDataBase', createData);
+  router.get('/test', testGetData)
+  router.get('/initDataBase', createData)
 
     // 权限相关
-    router.post('/auth/login', logIn);
-    router.post('/auth/regist', regist)
-    router.get('/auth/checkLogin', checkLogIn);
-    router.get('/auth/logOut', logOut);
-    router.get('/auth/user', getUser)
+  router.post('/auth/login', logIn)
+  router.post('/auth/regist', regist)
+  router.get('/auth/checkLogin', checkLogIn)
+  router.get('/auth/logOut', logOut)
+  router.get('/auth/user', getUser)
 
+  // 设备管理相关
+  router.get('/device/getAll/:page?', getAllDevice)
+  router.get('/device/onlyAll', onluGetAllDevice)
+  router.post('/device/add', addDevice)
+  router.post('/device/getById', getDeviceById)
+  router.post('/device/modifyById', modifyDeviceById)
+  router.post('/device/deleteById', delteDeviceById)
+  router.post('/device/getDeviceByTypeId', getDeviceByTypeId)
+  router.post('/device/search', deviceSearch)
 
-    // 设备管理相关
-    router.get('/device/getAll/:page?', getAllDevice);
-    router.get('/device/onlyAll', onluGetAllDevice);
-    router.post('/device/add', addDevice);
-    router.post('/device/getById', getDeviceById);
-    router.post('/device/modifyById', modifyDeviceById);
-    router.post('/device/deleteById', delteDeviceById);
-    router.post('/device/getDeviceByTypeId', getDeviceByTypeId);
-    router.post('/device/search', deviceSearch);
+  // 人员管理相关
+  router.get('/user/getAll/:page?', getAllUser)
+  router.get('/user/onlyAll', onlyGetAllUser)
+  router.post('/user/add', addUser)
+  router.post('/user/getById', getUserById)
+  router.post('/user/modifyById', modifyUserById)
+  router.post('/user/deleteById', deleteUserById)
+  router.post('/user/getPersonal', getPersonal)
+  router.post('/user/search', userSearch)
 
-    // 人员管理相关
-    router.get('/user/getAll/:page?', getAllUser);
-    router.get('/user/onlyAll', onlyGetAllUser);
-    router.post('/user/add', addUser);
-    router.post('/user/getById', getUserById);
-    router.post('/user/modifyById', modifyUserById);
-    router.post('/user/deleteById', deleteUserById);
-    router.post('/user/getPersonal', getPersonal)
-    router.post('/user/search', userSearch)
+  // 公告管理相关  router.get('/rule/getAll/:page?', getAllRules);
+  router.post('/rule/add', addRule)
+  router.post('/rule/getById', getRuleById)
+  router.post('/rule/modifyById', modifyRule)
+  router.post('/rule/deleteById', deleteRule)
+  router.post('/rule/search', ruleSearch)
 
-    // 公告管理相关
-    router.get('/rule/getAll/:page?', getAllRules);
-    router.post('/rule/add', addRule);
-    router.post('/rule/getById', getRuleById);
-    router.post('/rule/modifyById', modifyRule);
-    router.post('/rule/deleteById', deleteRule);
-    router.post('/rule/search', ruleSearch);
+  // 消息管理相关
+  router.get('/message/getAll/:page?', getAllMessage)
+  router.post('/message/add', addMessage)
+  router.post('/message/getById', getMessageById)
+  router.post('/message/modifyById', modifyMessageById)
+  router.post('/message/deleteById', deletseMessageById)
+  router.post('/message/search', messageSearch)
 
-    // 消息管理相关
-    router.get('/message/getAll/:page?', getAllMessage);
-    router.post('/message/add', addMessage);
-    router.post('/message/getById', getMessageById);
-    router.post('/message/modifyById', modifyMessageById);
-    router.post('/message/deleteById', deletseMessageById);
-    router.post('/message/search', messageSearch)
+  // 预约申请管理
+  router.get('/apply/getAll/:page?', getAllApply)
+  router.post('/apply/add', addApply)
+  router.post('/apply/getById', getApplyById)
+  router.post('/apply/modifyById', modifyApplyById)
+  router.post('/apply/deleteById', deleteApplyById)
+  router.post('/apply/addApplyFront', addApplyFront)
+  router.post('/apply/search', applySearch)
 
-    // 预约申请管理
-    router.get('/apply/getAll/:page?', getAllApply);
-    router.post('/apply/add', addApply);
-    router.post('/apply/getById', getApplyById);
-    router.post('/apply/modifyById', modifyApplyById);
-    router.post('/apply/deleteById', deleteApplyById);
-    router.post('/apply/addApplyFront', addApplyFront)
-    router.post('/apply/search', applySearch)
+  // 处理图片上传
+  router.post('/upload/imageUpload', upload.single('file'), imageUploadToTemp)
+  router.post('/upload/deleteTempFile', deleteTempFile)
+  router.post('/upload/copyTempFileToDir', copyTempFileToDir)
 
-    // 处理图片上传
-    router.post('/upload/imageUpload', upload.single('file'), imageUploadToTemp);
-    router.post('/upload/deleteTempFile', deleteTempFile);
-    router.post('/upload/copyTempFileToDir', copyTempFileToDir);
+  // 处理视频流
+  router.get('/video/getStream', checkFile, getVedioStream)
 
-    // 处理视频流
-    router.get('/video/getStream', checkFile, getVedioStream);
-
-    return router
-};
+  return router
+}
